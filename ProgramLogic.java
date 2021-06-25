422e164024cdab6881f0145cccc409375b5b05dc
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 16:47:01 +0530

    Update ProgramLogic.java

--- ProgramLogic.java
+++ ProgramLogic.java
@@ -1,1 +1,12 @@
+package com.Bridglab.programs
+import java.util.Scanner;
+import com.bridgebaz.utility.Utility
 
+public class Fibonacci {
+    public static void main(String[] args) {
+        Utility utility = new Utility();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number of series you want : ");
+        int num = s.nextInt();
+        utility.fibonacci(num);
+    }
