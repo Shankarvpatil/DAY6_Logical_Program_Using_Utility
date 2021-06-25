271ee88aad9b7d8a8503a2b76f5e633fbd12c6a1
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 17:11:19 +0530

    Update ProgramLogic.java

--- ProgramLogic.java
+++ ProgramLogic.java
@@ -5,7 +5,6 @@ public class PrimeNumber {
     public static void main(String[] args) {
     	Utility utility = new Utility();
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = s.nextInt();
+        int num = utility.getIntValue();
         utility.isPrime(num);
     }
