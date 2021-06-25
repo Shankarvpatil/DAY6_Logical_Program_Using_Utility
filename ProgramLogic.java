392865427f76497fcf768da9ee3bc4c9ebfd1394
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 17:13:06 +0530

    Update ProgramLogic.java

--- ProgramLogic.java
+++ ProgramLogic.java
@@ -5,15 +5,14 @@ public class PerfectNumber {
 	
 	public static void main(String[] args) {
 		Utility utility = new Utility();
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = s.nextInt();
-        int perfectNum = perfectNumber(num);
-        if (perfectNum == num){
-            System.out.println(num + " is a perfect number.");
-        }else{
-            System.out.println(num + " is not a perfect number.");
-	     }     
-    }
-   	 int num = utility.getIntValue();
- 	 utility.perfectNumber(num);
+        	Scanner s = new Scanner(System.in);
+        	int num1 = utility.getIntValue();
+        	int perfectNum = perfectNumber(num);
+        	if (perfectNum == num){
+            		System.out.println(num + " is a perfect number.");
+        	}else{
+           		System.out.println(num + " is not a perfect number.");
+	     	}     
+   	 }
+   	 	int num = utility.getIntValue();
+ 	 	utility.perfectNumber(num);
