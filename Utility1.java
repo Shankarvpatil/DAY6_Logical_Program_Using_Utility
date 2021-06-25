d81e03db8d9ead162b12db1a933e26cb80ad9f57
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 20:18:24 +0530

    Update Utility1.java

--- Utility1.java
+++ Utility1.java
@@ -72,4 +72,27 @@ private static int reverseNumber(int num) {
         }
         return rev;
     }
+//Stop watch program
+public void getElapsTime() {
+		int choice=0;
+	    double start=0, stop=0;
+        do {
+            System.out.println("1. Start");
+            System.out.println("2. Stop");
+            choice = getIntValue();
+            switch(choice) {
+       			case 1:
+       				start = System.currentTimeMillis();
+       				System.out.println("You have started the stopwatch please select stop now");
+       				break;
+       			case 2:
+       				stop = System.currentTimeMillis();
+       				break; 
+            }
+       }
+	while(choice != 2);
+       double elaps = ((stop - start)/1000)%60;
+       System.out.println("Elaps time = "+elaps+" sec");	
+	}
+}
 
