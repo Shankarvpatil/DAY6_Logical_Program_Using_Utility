fe9bceb04ecb4144da296f90824e0175dce196fd
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 17:01:12 +0530

    Update ProgramLogic.java

--- ProgramLogic.java
+++ ProgramLogic.java
@@ -1,12 +1,18 @@
 package com.Bridglab.programs
 import java.util.Scanner;
 import com.bridgebaz.utility.Utility
-
-public class Fibonacci {
-    public static void main(String[] args) {
-        Utility utility = new Utility();
+public class PerfectNumber {
+	
+	public static void main(String[] args) {
+		Utility utility = new Utility();
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number of series you want : ");
+        System.out.print("Enter a number: ");
         int num = s.nextInt();
-        utility.fibonacci(num);
+        int perfectNum = perfectNumber(num);
+        if (perfectNum == num)
+            System.out.println(num + " is a perfect number.");
+        else
+            System.out.println(num + " is not a perfect number.");
+            int num = utility.getIntValue();
+		    utility.perfectNumber(num);
     }
