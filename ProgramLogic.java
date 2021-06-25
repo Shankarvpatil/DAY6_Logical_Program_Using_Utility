163e1562d9c288dad6f74ec9744fb90a7d537d23
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 17:13:43 +0530

    Update ProgramLogic.java

--- ProgramLogic.java
+++ ProgramLogic.java
@@ -6,7 +6,6 @@ public class Fibonacci {
     public static void main(String[] args) {
         Utility utility = new Utility();
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number of series you want : ");
-        int num = s.nextInt();
+        int num = utility.getIntValue();
         utility.fibonacci(num);
     }
