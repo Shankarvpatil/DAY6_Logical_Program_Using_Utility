26317da5464e8c6701b1fbb2ad334d7546dbb229
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 20:35:17 +0530

    Update Utility1.java

--- Utility1.java
+++ Utility1.java
@@ -96,3 +96,30 @@ public void getElapsTime() {
 	}
 }
 
+public static void getCoupons(int noOfcoupns, int sizeOfCoupon) {
+		int cnt = 0;
+		String str1 = "";
+		String coupns = "";
+		while(cnt != noOfcoupns) {
+			str1 = generateCoupons(sizeOfCoupon);
+			if(!coupns.contains(str1)) {
+				coupns += str1;
+				cnt++;
+				System.out.println("coupon"+cnt+" =  "+str1);
+			}
+		}
+		
+	}
+
+	//generate random coupons
+
+	public static String generateCoupons(int sizeOfCoupon) {
+		String str = "";
+		Random random = new Random();
+			for(int i=0; i<sizeOfCoupon; i++) {
+				int num = random.nextInt(10);
+				str += ""+num;
+			}
+		return str;
+	}
+
