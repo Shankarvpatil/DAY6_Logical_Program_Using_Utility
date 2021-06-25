4f66f526100924b29a251e934a508c01a9df1e7b
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 17:06:21 +0530

    Update ProgramLogic.java

--- ProgramLogic.java
+++ ProgramLogic.java
@@ -1,19 +1,11 @@
 package com.Bridglab.programs
 import java.util.Scanner;
 import com.bridgebaz.utility.Utility
-public class PerfectNumber {
-	
-	public static void main(String[] args) {
-		Utility utility = new Utility();
+public class PrimeNumber {
+    public static void main(String[] args) {
+    	Utility utility = new Utility();
         Scanner s = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = s.nextInt();
-        int perfectNum = perfectNumber(num);
-        if (perfectNum == num){
-            System.out.println(num + " is a perfect number.");
-        }else{
-            System.out.println(num + " is not a perfect number.");
-	     }     
+        utility.isPrime(num);
     }
-   	 int num = utility.getIntValue();
- 	 utility.perfectNumber(num);
